cb7735c87f766c3fd26e7c07778829a86ed12b48
Pat Sissons <patricksissons@gmail.com>
2018-09-05 22:19:33 -0700

    tmp

--- src/index.ts
+++ src/index.ts
@@ -1,52 +1,53 @@
-import { GraphQLServer } from 'graphql-yoga'
-import { importSchema } from 'graphql-import'
-import { Prisma } from './generated/prisma'
-import { Context } from './utils'
+import { GraphQLServer } from "graphql-yoga";
+import { importSchema } from "graphql-import";
+import { Prisma } from "./generated/prisma";
+import { Context } from "./utils";
 
 const resolvers = {
   Query: {
-    feed(parent, args, context: Context, info) {
-      return context.db.query.posts({ where: { isPublished: true } }, info)
-    },
-    drafts(parent, args, context: Context, info) {
-      return context.db.query.posts({ where: { isPublished: false } }, info)
-    },
-    post(parent, { id }, context: Context, info) {
-      return context.db.query.post({ where: { id: id } }, info)
-    },
+    stations(parent, args, context: Context, info) {
+      return context.db.query.stations({}, info);
+    }
   },
   Mutation: {
-    createDraft(parent, { title, text }, context: Context, info) {
-      return context.db.mutation.createPost(
-        { data: { title, text } },
-        info,
-      )
+    createStation(parent, { name, lat, lng }, context: Context, info) {
+      return context.db.mutation.createStation(
+        { data: { name, lat, lng } },
+        info
+      );
     },
-    deletePost(parent, { id }, context: Context, info) {
-      return context.db.mutation.deletePost({ where: { id } }, info)
+    updateStationLocation(parent, { id, lat, lng }, context: Context, info) {
+      return context.db.mutation.updateStation(
+        { data: { name, lat, lng }, where: { id } },
+        info
+      );
     },
-    publish(parent, { id }, context: Context, info) {
-      return context.db.mutation.updatePost(
+    publishStationStatus(
+      parent,
+      { id, operative, total, free, bikes },
+      context: Context,
+      info
+    ) {
+      return context.db.mutation.createStationStatus(
         {
-          where: { id },
-          data: { isPublished: true },
+          data: { station: { connect: { id } }, operative, total, free, bikes }
         },
-        info,
-      )
-    },
-  },
-}
+        info
+      );
+    }
+  }
+};
 
 const server = new GraphQLServer({
-  typeDefs: './src/schema.graphql',
+  typeDefs: "./src/schema.graphql",
   resolvers,
   context: req => ({
     ...req,
     db: new Prisma({
-      endpoint: 'https://us1.prisma.sh/public-cypresseagle-956/where-my-mobi/dev', // the endpoint of the Prisma API
-      debug: true, // log all GraphQL queries & mutations sent to the Prisma API
+      endpoint: "https://us1.prisma.sh/pat-sissons-fe92fd/where-my-mobi/dev", // the endpoint of the Prisma API
+      debug: true // log all GraphQL queries & mutations sent to the Prisma API
       // secret: 'mysecret123', // only needed if specified in `database/prisma.yml`
-    }),
-  }),
-})
-server.start(() => console.log('Server is running on http://localhost:4000'))
+    })
+  })
+});
+server.start(() => console.log("Server is running on http://localhost:4000"));
