36b7d3767b65d84a2c700c8325308964b20cc8f5
Pat Sissons <pat.sissons@shopify.com>
2019-03-26 23:05:46 -0700

    fixing declaration file

--- config/types/declarations.d.ts
+++ config/types/declarations.d.ts
@@ -1,4 +1,29 @@
+declare global {
+  export const google: any;
+}
+
 declare module '*.scss' {
   const content: any;
   export = content;
+}
+
+declare module 'react-google-maps' {
+  export class BicyclingLayer extends React.PureComponent {}
+}
+
+declare module 'react-google-maps/lib/constants' {
+  export const MAP: string;
+}
+
+declare module 'recompose' {
+  import {Observable} from 'rxjs';
+
+  export function fromRenderProps<TInner, TOuter>(
+    component: React.ComponentType<React.ConsumerProps<TInner>>,
+    propsMapper: mapper<TInner, TOuter>,
+  ): InferableComponentEnhancerWithProps<TInner & TOuter, TOuter>;
+
+  export function mapPropsStream<TInner, TOutter>(
+    transform: mapper<Observable<TOutter>, Observable<TInner>>,
+  ): ComponentEnhancer<TInner, TOutter>;
 }
