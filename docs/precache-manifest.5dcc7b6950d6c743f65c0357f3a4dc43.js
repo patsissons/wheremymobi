d8e9ec7358a0b6e61f4d734e9ac2fd80dcde1bd1
Pat Sissons <pat.sissons@shopify.com>
2019-06-15 00:52:07 -0700

    Publish

---
+++ docs/precache-manifest.5dcc7b6950d6c743f65c0357f3a4dc43.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d715dfd164e1bbfee41471a67f4b5e40",
+    "url": "/index.html"
+  },
+  {
+    "revision": "53b5d648c077fa1d6b4f",
+    "url": "/static/css/2.055adff4.chunk.css"
+  },
+  {
+    "revision": "48e5d0207ee831a13836",
+    "url": "/static/css/main.bbe2a0e4.chunk.css"
+  },
+  {
+    "revision": "53b5d648c077fa1d6b4f",
+    "url": "/static/js/2.9d588c54.chunk.js"
+  },
+  {
+    "revision": "48e5d0207ee831a13836",
+    "url": "/static/js/main.d39ff02b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "44bc6267b4f596bf390cdfd25ddee652",
+    "url": "/static/media/bike.44bc6267.svg"
+  },
+  {
+    "revision": "9f3589a23afe0efe53cdef3bad2dbbf5",
+    "url": "/static/media/gps.9f3589a2.svg"
+  },
+  {
+    "revision": "137b889f4bd98943ee54a1657da796c1",
+    "url": "/static/media/refresh.137b889f.svg"
+  },
+  {
+    "revision": "ae84d45d79b997ff981014f86267132e",
+    "url": "/static/media/slot.ae84d45d.svg"
+  }
+]);
