c16bf0c0127b0d45107c5db54bcda500cde4448e
Pat Sissons <pat.sissons@shopify.com>
2021-07-25 10:00:45 -0700

    fixing .env

--- src/utilities/env.ts
+++ src/utilities/env.ts
@@ -1,5 +1,4 @@
 export const {
-  // GOOGLE_MAPS_API_KEY = 'AIzaSyBXrYScIU6sWYUWLLlovYhzq-bLzwTgAoc',
-  GOOGLE_MAPS_API_KEY = 'AIzaSyBqnHB52AARBl2t1vG3kkY0q6obTTMyHVQ',
+  GOOGLE_MAPS_API_KEY,
   // eslint-disable-next-line no-process-env
 } = process.env;
