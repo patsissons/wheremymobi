e6802077faaf25bc869e52f942e3772ccf283892
Pat Sissons <pat.sissons@shopify.com>
2021-07-25 12:22:33 -0700

    remove redundant typescript config

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,11 +1,1 @@
 /// <reference types="react-scripts" />
-
-declare module '*.png' {
-  const content: any;
-  export = content;
-}
-
-declare module '*.scss' {
-  const content: any;
-  export = content;
-}
