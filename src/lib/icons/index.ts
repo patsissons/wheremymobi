c6e80f397a23b3e033a64d141c363fbda205a2b5
Pat Sissons <patricksissons@gmail.com>
2023-07-02 12:10:45 -0700

    adding new social media icons
    - `GithubIcon`
    - `TwitterIcon`

--- src/lib/icons/index.ts
+++ src/lib/icons/index.ts
@@ -1,6 +1,8 @@
 export { default as CaretCollapsedIcon } from './caret-collapsed.svelte';
 export { default as CaretExpandedIcon } from './caret-expanded.svelte';
 export { default as DirectionsIcon } from './directions.svelte';
+export { default as GithubIcon } from './github.svelte';
 export { default as GoogleMapsIcon } from './google-maps.svelte';
 export { default as MarkerIcon } from './marker.svelte';
 export { default as RefreshIcon } from './refresh.svelte';
+export { default as TwitterIcon } from './twitter.svelte';
